a56725cca64b633f9903428544986fadff83051e
Harrison Short <wot@google.com>
2024-05-06 15:33:47 +1000

    Add KeyCollision error to mapchains Transform
    When there is a collision with the newly transformed key, return an
    error if it is not unique within the transformed map.
    
    Since maps do not have a deterministic iteration order, not throwing an
    error here would result in non-deterministic behaviour if there is a
    collision.

--- chains/mapchains/maps.go
+++ chains/mapchains/maps.go
@@ -1,14 +1,24 @@
 package mapchains
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/kagadar/go-pipeline/chains"
 )
+
+var errKeyCollision = errors.New("key already in map")
+
+func IsKeyCollisionErr(err error) bool {
+	return errors.Is(err, errKeyCollision)
+}
 
 func propagateErr[K comparable, V any](err error) chains.MapLink[K, V] {
 	return func() (map[K]V, error) { return nil, err }
 }
 
-// Transform uses the provided function to transform the key-value pairs of the provided MapLink into a new MapLink.
+// Transform uses the provided function to transform the key-value pairs of the
+// provided MapLink into a new MapLink, with new key-value types.
 func Transform[K1, K2 comparable, V1, V2 any](i chains.MapLink[K1, V1], f func(K1, V1) (K2, V2, error)) chains.MapLink[K2, V2] {
 	in, err := i()
 	if err != nil {
@@ -19,6 +29,9 @@ func Transform[K1, K2 comparable, V1, V2 any](i chains.MapLink[K1, V1], f func(K
 		k, v, err := f(k, v)
 		if err != nil {
 			return propagateErr[K2, V2](err)
+		}
+		if _, ok := o[k]; ok {
+			return propagateErr[K2, V2](fmt.Errorf("%v: %w", k, errKeyCollision))
 		}
 		o[k] = v
 	}
--- chains/mapchains/maps_test.go
+++ chains/mapchains/maps_test.go
@@ -26,3 +26,13 @@ func TestTransform(t *testing.T) {
 		t.Errorf("Transform() unexpected diff (-got +want):\n%s", diff)
 	}
 }
+
+func TestTransform_ErrKeyCollision(t *testing.T) {
+	_, err := Transform(
+			chains.NewMapLink(map[int]string{1: "1", 2: "1", 3: "3"}),
+			func(k int, v string) (string, int, error) { return v, k, nil },
+		)()
+	if !IsKeyCollisionErr(err) {
+		t.Errorf("Transform() error = %v, want %v", err, errKeyCollision)
+	}
+}
